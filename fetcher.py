7bcf098fb86534caec929eef6b03e3ff459efc1c
Stefano Taillefert <stefano.taille@gmail.com>
2023-06-25 21:19:55 +0000

    Update exception

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,6 @@
 from os import getenv
 
 from requests import get
-from requests.exceptions import JSONDecodeError
 
 from data import bot_name
 
@@ -13,10 +12,8 @@ if api_url == None:
 # Get new tweets (more recent than newest_id)
 def fetch_tweets(newest_id: str) -> list:
     response = get(f"{api_url}?newestId={newest_id}")
-    try:
-        data = response.json()
-    except JSONDecodeError:
-        raise Exception("Unexpected JSON data")
+    data = response.json()
+
     new_tweets = len(data)
     if new_tweets != 0:
         newest_id = data[-1]["id"]
